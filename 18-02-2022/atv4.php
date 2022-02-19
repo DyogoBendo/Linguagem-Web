3089c26b58182bf1965a3073a25b945b9dcf40ba
DyogoBendo <dyogoromagnabendo@gmail.com>
2022-02-19 18:27:08 -0300

    atualizando matrizes

--- 18-02-2022/atv4.php
+++ 18-02-2022/atv4.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matriz</title>    
+</head>
+<body>
+    <?php
 
     function multiplica_vetor($vetor1, $vetor2){
         echo "<table>";
@@ -15,6 +24,7 @@
     $vetor1 = [1, 3, 5];
     $vetor2 = [1, 3, 5];    
 
-    multiplica_vetor($vetor1, $vetor2);
-
-    echo "oi";
+    multiplica_vetor($vetor1, $vetor2);    
+    ?>
+</body>
+</html>
